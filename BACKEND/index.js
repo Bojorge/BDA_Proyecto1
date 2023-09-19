baa0b35efbbdfc70ee66ce2bc351f10b922c4ac7
Bojtronic <bojorgearayamanuel@gmail.com>
2023-09-19 16:36:50 -0600

    Modularizando API

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const neo4j = require("./database");
+
+const graphRoute = require('./src/routes/graph_route');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,18 +10,11 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get("/consulta", async (req, res) => {
-  const session = neo4j.session();
 
-  try {
-    const result = await session.run("MATCH (n) RETURN n LIMIT 25");
-    res.json(result.records.map((record) => record.get(0).properties));
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error en la consulta" });
-  } finally {
-    session.close();
-  }
-});
+app.use('/api/consulta', graphRoute);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+
--- FRONTEND/Scripts/get_test.js
+++ FRONTEND/Scripts/get_test.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:3000';
+const apiUrl = 'http://localhost:3000/api';
 
 document.addEventListener('DOMContentLoaded', () => {
   const getUsersButton = document.getElementById('getUsersButton');
