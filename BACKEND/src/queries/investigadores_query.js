7f8d61933f1964d5088175548779430dc0988c77
Bojtronic <bojorgearayamanuel@gmail.com>
2023-09-21 19:07:13 -0600

    CRUD de investigadores

--- BACKEND/src/queries/investigadores_query.js
+++ BACKEND/src/queries/investigadores_query.js
@@ -1,7 +1,9 @@
+const get = "MATCH (i:Investigador) RETURN i";
 const add = "CREATE (i:Investigador {id: $id, nombre_completo: $nombre_completo, titulo_academico: $titulo_academico, institucion: $institucion, email: $email})";
 const update = "MATCH (i:Investigador {id: $id}) SET i.nombre_completo = $nombre_completo, i.titulo_academico = $titulo_academico, i.institucion = $institucion, i.email = $email";
 
 module.exports = {
+    get,
     add,
     update
 }
