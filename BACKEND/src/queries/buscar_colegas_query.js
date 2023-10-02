d27b88f1f4b26b54585cea48a667ca366df1b2ce
Bojtronic <bojorgearayamanuel@gmail.com>
2023-10-02 15:52:30 -0600

    Busqueda de colegas

--- BACKEND/src/queries/buscar_colegas_query.js
+++ BACKEND/src/queries/buscar_colegas_query.js
@@ -1,4 +1,7 @@
-const get = "MATCH (i:Investigador {id: $investigadorId})-[:PARTICIPA_EN]->(p:Proyecto)<-[:PARTICIPA_EN]-(colega:Investigador) WHERE colega <> i RETURN DISTINCT colega.nombre_completo AS nombre_colega;"
+const get = "MATCH (i:Investigador {id: $investigadorId})-[:PARTICIPA_EN]->(p:Proyecto)<-[:PARTICIPA_EN]-(colega:Investigador) WHERE colega <> i WITH COLLECT(DISTINCT colega) AS colegas RETURN colegas;"
+
+
+
 
 
 module.exports = {
